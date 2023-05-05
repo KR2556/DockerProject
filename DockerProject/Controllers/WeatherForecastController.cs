3a89612fe0e9f534a1b1db75f62f08d13218bed8
U_M1HRZ <U_M1HRZ@HNBA118456>
2023-05-05 14:31:54 +0300

    asf

--- DockerProject/Controllers/WeatherForecastController.cs
+++ DockerProject/Controllers/WeatherForecastController.cs
@@ -8,6 +8,7 @@ namespace DockerProject.Controllers
     
     public class WeatherForecastController : ControllerBase
     {
+        
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
