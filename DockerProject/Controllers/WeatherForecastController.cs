c1f211809a29c6f3da843ea6efcd0014294a9d04
U_M1HRZ <U_M1HRZ@HNBA118456>
2023-05-05 14:50:19 +0300

    TEST

--- DockerProject/Controllers/WeatherForecastController.cs
+++ DockerProject/Controllers/WeatherForecastController.cs
@@ -16,6 +16,7 @@ namespace DockerProject.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
+       
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
