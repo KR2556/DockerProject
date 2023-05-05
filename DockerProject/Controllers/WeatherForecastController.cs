40617df312791ff95bce578e6a0c1443acad0040
U_M1HRZ <U_M1HRZ@HNBA118456>
2023-05-05 14:31:03 +0300

    psi

--- DockerProject/Controllers/WeatherForecastController.cs
+++ DockerProject/Controllers/WeatherForecastController.cs
@@ -4,6 +4,8 @@ namespace DockerProject.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    
+    
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
